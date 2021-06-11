4891bf4ed715cc9680ba7b334fbcd7e1417322d2
Matthijs Kotterink <poukimonster@gmail.com>
2021-06-11 14:02:46 +0200

    added Registration

--- demo/src/main/java/openRemote/demo/API/UserController.java
+++ demo/src/main/java/openRemote/demo/API/UserController.java
@@ -3,11 +3,9 @@ package openRemote.demo.API;
 import openRemote.demo.Model.LoginModel;
 import openRemote.demo.Model.UserModel;
 import openRemote.demo.Repository.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -19,5 +17,13 @@ public class UserController {
     @PostMapping("/Login")
     public UserModel Login(@RequestBody LoginModel model){
         return repository.FindByName(model);
+    }
+
+    @PostMapping("register/{userid}")
+    public void Login(@RequestBody UserModel newUser, @PathVariable ObjectId userid){
+        UserModel currUser = repository.findById(userid).orElse(null);
+        if(currUser.accessLevel >= 2){
+            repository.insert(newUser);
+        }
     }
 }
